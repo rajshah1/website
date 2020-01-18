7c1925a860afcf79af1277fda12856f76f876563
rajshah1 <shahraj121997@gmail.com>
2020-01-19 01:11:07 +0530

    added more logics

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,36 @@
-function myFunction() {
-    var x = document.getElementsByClassName("navlinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+function menu_click(){
+    var check_menu=document.getElementById("navlinks");
+    var class_lines=document.getElementsByClassName("line");
+    var class_close=document.getElementsByClassName("close");
+    console.log(class_close,class_close.length);
+    if (check_menu.style.display === "block"){
+        check_menu.style.display = "none";
+        for(i=0;i<class_lines.length;i++){
+            class_lines[i].style.display = "block";
+        }
+       class_close[0].style.display = "none";
     }
-  }
+    else{
+        check_menu.style.display = "block";
+        for(i=0;i<class_lines.length;i++){
+            class_lines[i].style.display = "none";
+        }
+        class_close[0].style.display = "block";
+
+    }
+
+     
+    /*
+    var check_display = getComputedStyle(document.querySelector("#navlinks"));
+    if (check_display.display==="block"){;
+        check_display.display="none";
+    }
+    else{
+        check_display.display = "block"; 
+    }
+    if(check_display.style.display==="none"){
+        check_display.style.display="block";
+    }*/
+    //check_display.namedItem(display);.
+
+}
