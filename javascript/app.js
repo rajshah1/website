49a3a51d511a430e0ad1a0b74efa1f17872d1b68
rajshah1 <shahraj121997@gmail.com>
2020-01-21 23:07:27 +0530

    added pre_loader

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,37 @@
+function myFunction() {
+    setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    var classPreLoader = document.getElementById("myDiv");
+    classPreLoader.style.display="block";   
+}
+
+
+
 function menu_click(){
-    var check_menu=document.getElementById("navlinks");
+    var links=document.getElementById("navlinks");
     var class_lines=document.getElementsByClassName("line");
     var class_close=document.getElementsByClassName("close");
-    console.log(class_close,class_close.length);
-    if (check_menu.style.display === "block"){
-        check_menu.style.display = "none";
+    
+    if (links.style.display === "block"){
+        links.style.display = "none";
         for(i=0;i<class_lines.length;i++){
             class_lines[i].style.display = "block";
         }
        class_close[0].style.display = "none";
     }
     else{
-        check_menu.style.display = "block";
+        links.style.display = "block";
         for(i=0;i<class_lines.length;i++){
             class_lines[i].style.display = "none";
         }
         class_close[0].style.display = "block";
+        
 
     }
-
+}
      
     /*
     var check_display = getComputedStyle(document.querySelector("#navlinks"));
@@ -33,4 +46,3 @@ function menu_click(){
     }*/
     //check_display.namedItem(display);.
 
-}
