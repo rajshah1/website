24c460107af67c95166e31c12552b5ca55924b18
rajshah1 <shahraj121997@gmail.com>
2020-02-13 01:51:53 +0530

    added resume page

--- javascript/app.js
+++ javascript/app.js
@@ -9,6 +9,24 @@ function showPage() {
     var classPreLoader = document.getElementById("myDiv");
     classPreLoader.style.display="block";   
 }
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500|| document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
