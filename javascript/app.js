152f5b7ac294ac3dcdbc48ed7ccfc4706ee68060
rajshah1 <shahraj121997@gmail.com>
2020-02-14 02:52:14 +0530

    added fourth page

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,3 @@
-
-
 function myFunction() {
     setTimeout(showPage, 3000);
 }
@@ -7,48 +5,47 @@ function myFunction() {
 function showPage() {
     document.getElementById("loader").style.display = "none";
     var classPreLoader = document.getElementById("myDiv");
-    classPreLoader.style.display="block";   
+    classPreLoader.style.display = "block";
 }
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500|| document.documentElement.scrollTop > 500) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 
 
-function menu_click(){
-    var links=document.getElementById("navlinks");
-    var class_lines=document.getElementsByClassName("line");
-    var class_close=document.getElementsByClassName("close");
-    
-    if (links.style.display === "block"){
+function menu_click() {
+    var links = document.getElementById("navlinks");
+    var class_lines = document.getElementsByClassName("line");
+    var class_close = document.getElementsByClassName("close");
+
+    if (links.style.display === "block") {
         links.style.display = "none";
-        for(i=0;i<class_lines.length;i++){
+        for (i = 0; i < class_lines.length; i++) {
             class_lines[i].style.display = "block";
         }
-       class_close[0].style.display = "none";
-    }
-    else{
+        class_close[0].style.display = "none";
+    } else {
         links.style.display = "block";
-        for(i=0;i<class_lines.length;i++){
+        for (i = 0; i < class_lines.length; i++) {
             class_lines[i].style.display = "none";
         }
         class_close[0].style.display = "block";
-        
+
 
     }
 }
